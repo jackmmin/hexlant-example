1ee70e4487e3ca39665925440007524e401f07bc
jackmmin <dpeltms14@naver.com>
2019-08-07 15:29:13 +0900

    mod test.js_함수 호출법 -3까지

--- project/test/test.js
+++ project/test/test.js
@@ -1,8 +1,8 @@
 const contractName = artifacts.require( 'mytoken' );
 
-contract( "mytoken contract test", async function ( accounts ) {
+contract( "mytoken contract test", async function ( [철수, 영희, 민희] ) {    // 
     before( async function () {
-        this.contract = await contractName.new();
+        this.contract = await contractName.new(); // 배포할 특정 account를 지정할때 from: account로 지정해준다.
     })
 
     describe( "함수 호출 법", function () {
@@ -24,15 +24,24 @@ contract( "mytoken contract test", async function ( accounts ) {
 
     describe( "함수 호출 법 - 2", function() {
         it( "파라미터가 존재하는 함수 호출", async function () {
-                var account_balance_0 = await this.contract.balanceOf( accounts[ 0 ] );  // ganache-cle에서 accounts를 확인할 수 있다.
-                console.log( parseInt( account_balance_0 ) );
-                var account_balance_1 = await this.contract.balanceOf( accounts[ 1 ] );
-                console.log( parseInt( account_balance_1 ) );
-                
-                // for( var i=0; i<9; i++ ) {
-                //     var account_balance = await this.contract.balanceOf( accounts[ i ] );
-                //     console.log( parseInt( account_balance[i] ) );
-                // }
+                var account_balance = await this.contract.balanceOf( 철수 ); 
+                console.log( parseInt( account_balance ) );
+        })
+    })
+
+    describe( "함수 호출 법 -3", function() {
+        it( "서명을 필요로 하는 함수 호출", async function() {
+            var 영희토큰잔액 = await this.contract.balanceOf( 영희 );
+            console.log( '영희토큰잔액', parseInt( 영희토큰잔액 ) );
+            var 철수토큰잔액 = await this.contract.balanceOf( 철수 );
+            console.log( 철수토큰잔액 );
+
+            await this.contract.transfer( 영희, 100, { from: 철수 } ); 
+
+            var 영희토큰잔액 = await this.contract.balanceOf( 영희 );
+            console.log( '영희토큰잔액', parseInt( 영희토큰잔액 ) );
+            var 철수토큰잔액 = await this.contract.balanceOf( 철수 );
+            console.log( 철수토큰잔액 );
         })
     })
 })
